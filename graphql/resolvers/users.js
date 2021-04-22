50ecd6045d001f4f6f68b9946c94cf63385d23ef
Brittanie <brittanie.boyko@gmail.com>
2021-04-22 08:32:34 -0400

    used bcrypt and json web tokens to hash user's password and create unique token

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,45 @@
-const User = require('../../models/User');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = require("../../config");
+const User = require("../../models/User");
 
 module.exports = {
-    Mutation: {
-        register(_, args, context, info){
-            //TODO: Validate user data
-            //TODO: Make suer user doesn't already exist
-            //TODO: Hash password and create auth token
-        }
-    }
-}
+  Mutation: {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      //TODO: Validate user data
+      //TODO: Make suer user doesn't already exist
+      //TODO: Hash password and create auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+      const res = await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
