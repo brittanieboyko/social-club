7bae7dde636042d62941f924285bba7d0aeccd3c
Brittanie <brittanie.boyko@gmail.com>
2021-04-23 08:20:19 -0400

    pass in user email and password data to validate

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
+const { validateRegisterInput } = require("../../util/validators");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
@@ -12,6 +13,10 @@ module.exports = {
       { registerInput: { username, email, password, confirmPassword } }
     ) {
       //TODO: Validate user data
+      const {valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
+      if (!valid) {
+        throw new UserInputError("Errors", { errors })
+      }
       //TODO: Make suer user doesn't already exist
       const user = await User.findOne({ username });
       if (user) {
